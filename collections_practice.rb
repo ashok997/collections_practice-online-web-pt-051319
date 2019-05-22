cd0d3135ea41eee3358d845255e8c73e5df64f02
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-22 21:43:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
 
+def sort_array_asc(numbers_array)
+  numbers_array.sort
+end
+
+def sort_array_desc (numbers_array)
+  numbers_array.sort{|a,b| b<=>a}
+end
+
+def sort_array_char_count(string_array)
+  string_array.sort do |a,b|
+    a.length <=>b.length
+  end
+end
+
+def swap_elements(array)
+  array[1],array[2] = array[2],array[1]
+  array
+end
+
+def reverse_array(numbers_array)
+  numbers_array.reverse
+end
+
+def kesha_maker(names_array)
+  names_array.each{|name| name[2] ="$"}
+end
